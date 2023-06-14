7591dfb0db66f77475aa5fdab0709fd0413a330d
KAMAL SANJAY <89210602+kamalsanjay@users.noreply.github.com>
2023-06-14 18:58:53 +0530

    Add files via upload

---
+++ heart.py
@@ -0,0 +1,19 @@
+from turtle import *
+import turtle
+
+wn=turtle.Screen()
+# wn.bgcolor('blue')
+wn.colormode(255)
+wn.bgcolor(255,255,255)
+color('red')
+begin_fill()
+pensize(6)
+
+left(50)
+forward(163)
+circle(70,205)
+
+right(140)
+circle(70,205)
+forward(163)
+end_fill()
